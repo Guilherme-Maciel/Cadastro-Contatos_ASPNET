7752ab4d5d191b0efa8ac08d366a1289b571cf66
guilherme.maciel <guilherme.maciel@BRSPNOTE-38>
2022-04-16 16:21:47 -0300

    Adicionar arquivos de projeto.

---
+++ contactSystem/Controllers/ContatoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace contactSystem.Controllers
+{
+    public class ContatoController : Controller
+    {
+        //Método principal da controller
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        {
+            return View();
+        }
+        public IActionResult Excluir()
+        {
+            return View();
+        }
+    }
+}
